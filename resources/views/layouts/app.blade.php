ed426850cdb3ea2cac2d0025bac5df93b22925f0
eugen90 <euugen_90@yahoo.com>
2020-12-02 22:11:48 +0200

    Responsive for mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -105,11 +107,8 @@
             @yield('content')
         </main>
     </div>
-    <hr>
-    <!-- Footer -->
     <footer class="page-footer font-small blue pt-4">
-
-        <!-- Footer Links -->
+        <hr>
         <div class="container-fluid text-center text-md-left">
 
             <!-- Grid row -->
