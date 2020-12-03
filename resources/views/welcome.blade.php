ae2b176efc2df49162670ae7d99d24545c2ff0ef
eugen90 <euugen_90@yahoo.com>
2020-12-03 13:37:54 +0200

    Finished footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,179 +1,53 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Car dealer') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
-    <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}" />
-</head>
-
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="https://us.123rf.com/450wm/sergeypykhonin/sergeypykhonin1808/sergeypykhonin180800095/108230255-stock-vector-sport-car-logo-or-icon-rally-garage-symbol-vector-illustration.jpg?ver=6"
-                        width="90" height="60" alt="">
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('welcome') }}">{{ __('Home') }}</a>
-                        </li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('cars.index') }}">{{ __('Cars') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('contact') }}">{{ __('Contact') }}</a>
-                        </li>
-                        @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('cars.index') }}">{{ __('Cars') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('contact') }}">{{ __('Contact') }}</a>
-                        </li>
-                        @if(Auth::user()->role == 'admin')
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('admin') }}">{{ __('Admin') }}</a>
-                        </li>
-                        @endif
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <main class="py-4">
-            @yield('content')
-        </main>
+<div class="container py-5">
+    <div class="jumbotron text-white jumbotron-image shadow">
+        <h2 class="mb-4">
+            Parc Auto Bals
+        </h2>
+        <p class="mb-4">
+            Hey, check this out.
+        </p>
+        <a href="https://bootstrapious.com/snippets" class="btn btn-primary">More Bootstrap Snippets</a>
     </div>
-    <footer class="page-footer font-small blue pt-4">
-        <hr>
-        <div class="container-fluid text-center text-md-left">
-
-            <!-- Grid row -->
-            <div class="row">
-
-                <!-- Grid column -->
-                <div class="col-md-6 mt-md-0 mt-3">
-
-                    <!-- Content -->
-                    <h5 class="text-uppercase">Contact us</h5>
-                    <p>Our team is here for you..</p>
-
+</div>
+<div class="card-header">
+    <h1 class="card text-left">Recent :</h1>
+</div>
+<div class="album py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            @if(!empty($images))
+            @foreach($images as $img)
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" src="{{ asset('images/' . $img->file_name)}}">
+                    <div class="card-body">
+                        <p class="card-text">{{ $img->model }}</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="{{ route('cars.show', $img->id) }}" class="btn btn-primary"><i
+                                        class="fa fa-info-circle" aria-hidden="true"> Details</i></a>
+                            </div>
+                            <small class="text-muted">Last update: {{ $img->updated_at }} </small>
+                        </div>
+                    </div>
                 </div>
-                <!-- Grid column -->
-
-                <hr class="clearfix w-100 d-md-none pb-3">
-
-                <!-- Grid column -->
-                <div class="col-md-3 mb-md-0 mb-3">
-
-                    <!-- Links -->
-                    <h5 class="text-uppercase">Social media :</h5>
-
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="https://www.facebook.com/Parc-Auto-Bals-1550528411629952"><i
-                                    class="fab fa-facebook-square" style="font-size:36px"></i></a>
-                        </li>
-                        <li>
-                            <a href="#!"><i class="fab fa-whatsapp" style=" font-size:36px;color:green"></i></a>
-                        </li>
-                    </ul>
-
-                </div>
-                <!-- Grid column -->
-
-                <!-- Grid column -->
-                <div class="col-md-3 mb-md-0 mb-3">
-
-                    <!-- Links -->
-                    <h5 class="text-uppercase">About :</h5>
-
-                    <ul class="list-unstyled">
-                        <li>
-                            <p>Email : test@yahoo.com</p>
-                        </li>
-                        <li>
-                            <p>Phone : 07xx xxx xxx</p>
-                        </li>
-                    </ul>
-
-                </div>
-                <!-- Grid column -->
-
             </div>
-            <!-- Grid row -->
-
+            @endforeach
+            @else
+            <div class="card-header">
+                <h1 class="card text-left">No cars available</h1>
+            </div>
+            @endif
         </div>
-        <!-- Footer Links -->
-
-        <!-- Copyright -->
-        <div class="footer-copyright text-center py-3">© <?php echo date("Y"); ?> Copyright.
-        </div>
-        <!-- Copyright -->
-
-    </footer>
-    <!-- Footer -->
+    </div>
+</div>
+</main>
 </body>
 
 </html>
+
+@endsection
